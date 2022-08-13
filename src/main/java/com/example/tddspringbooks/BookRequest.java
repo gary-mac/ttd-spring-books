b5f7924ebbf843ce696832444acefff4bcbb35c7
Gary M <garymaccabe93@gmail.com>
2022-08-14 00:45:04 +0100

    Add test for creating new book entry

---
+++ src/main/java/com/example/tddspringbooks/BookRequest.java
@@ -0,0 +1,21 @@
+package com.example.tddspringbooks;
+
+public class BookRequest {
+
+
+    public void setAuthor(String name) {
+
+    }
+
+    public void setTitle(String s) {
+    }
+
+    public void setIsbn(String s) {
+    }
+
+    public static class BookService {
+        public Object createNewBook(BookRequest capture) {
+            return null;
+        }
+    }
+}
--- src/test/java/com/example/tddspringbooks/BookControllerTest.java
+++ src/test/java/com/example/tddspringbooks/BookControllerTest.java
@@ -1,17 +1,44 @@
 package com.example.tddspringbooks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(BookController.class)
 @ExtendWith(SpringExtension.class)
 class BookControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Captor
+	private ArgumentCaptor<BookRequest> argumentCaptor;
+
+	@MockBean
+	private BookRequest.BookService bookService;
+
 	@Test
-	void contextLoads() {
+	void testCreating_newBookEntry() {
+
+		BookRequest bookRequest = new BookRequest();
+		bookRequest.setAuthor("");
+		bookRequest.setTitle("");
+		bookRequest.setIsbn("");
+
+		when(bookService.createNewBook(argumentCaptor.capture())).thenReturn(1L);
 	}
 
 }
